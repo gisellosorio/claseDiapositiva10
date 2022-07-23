29d34856c568479183fb486609065adbef342455
gisellosorio <62785503+gisellosorio@users.noreply.github.com>
2022-07-23 11:27:13 -0700

    Ejemplo lectura de archivos por caracteres

--- src/App.java
+++ src/App.java
@@ -13,24 +13,24 @@ public class App {
         // //archivo.escribir("ejemploG18"); 
         // archivo.escribir("ejemploG18_comma.txt");
         
-        var ep=new ElemenPersona();
-        //puedo generar personas.dat, personas.txt, personas.obj
-        ep.guardarP("Personas.txt",
-                // convertir a list
-                Arrays.asList(
-                new Persona("552871883","María", "Ruiz Ramos"),
-                new Persona("403020104","Juan", "González López"),
-                new Persona("441234789","Luis", "Perez"),
-                new Persona("441234789","Luis", "Perez")
-                )); //Si voy agregando nuevos elementos, cambia la serie de los objetos. Si no los modifico, la serialización queda igual
-                //archivoob.Persona@573fd745
-                //archivoob.Persona@15327b79
-                //archivoob.Persona@4f2410ac
-                //archivoob.Persona@722c41f4
-                
-        List<Persona> personas=ep.cargarPersonas("Personas.txt");
-        personas.forEach(System.out::println);
+        // var ep=new ElemenPersona();
+        // //puedo generar personas.dat, personas.txt, personas.obj
+        // ep.guardarP("Personas.txt",
+        //         // convertir a list
+        //         Arrays.asList(
+        //         new Persona("552871883","María", "Ruiz Ramos"),
+        //         new Persona("403020104","Juan", "González López"),
+        //         new Persona("441234789","Luis", "Perez"),
+        //         new Persona("441234789","Luis", "Perez")
+        //         )); //Si voy agregando nuevos elementos, cambia la serie de los objetos. Si no los modifico, la serialización queda igual
+        //         //archivoob.Persona@573fd745
+        //         //archivoob.Persona@15327b79
+        //         //archivoob.Persona@4f2410ac
+        //         //archivoob.Persona@722c41f4
+
+        // List<Persona> personas=ep.cargarPersonas("Personas.txt");
+        // personas.forEach(System.out::println);
         
-        //Ejercicio1.run_Eje1();
+        Ejercicio1.run_Eje1();
 }
 }
